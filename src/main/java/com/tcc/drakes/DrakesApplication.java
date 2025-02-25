465fa326281d362a3667eddabb51e69059ede31c
isaacdev07 <isaacsantos7953@gmail.com>
2025-02-25 14:33:10 -0300

    feat: Initial commit Hello World

---
+++ src/main/java/com/tcc/drakes/DrakesApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.drakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrakesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/drakes/DrakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.drakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
