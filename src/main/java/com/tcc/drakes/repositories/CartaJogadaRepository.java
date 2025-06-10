3a23d35ba47dfcb5b185bbea379659fed53f4a4d
isaacdev07 <isaacsantos7953@gmail.com>
2025-06-10 13:35:58 -0300

    feat: cartas features deletadas

--- src/main/java/com/tcc/drakes/repositories/CartaJogadaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcc.drakes.repositories;
-
-import com.tcc.drakes.entities.CartaJogada;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartaJogadaRepository extends JpaRepository<CartaJogada, Long> {
-}
