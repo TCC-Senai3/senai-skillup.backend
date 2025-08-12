ee93e3906de53a223816f263044ee162161a0fbd
isaacdev07 <isaacsantos7953@gmail.com>
2025-08-12 08:02:55 -0300

    adição do cors no UsuarioController

--- src/main/java/com/tcc/drakes/controllers/UsuarioController.java
+++ src/main/java/com/tcc/drakes/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.tcc.drakes.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import com.tcc.drakes.entities.Usuario;
 import com.tcc.drakes.services.UsuarioService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
