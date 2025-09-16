2f060016ba59015621bb83c259831267b75a3f55
isaacdev07 <isaacsantos7953@gmail.com>
2025-09-16 14:51:15 -0300

    novo dto para lista de usuarios

---
+++ src/main/java/com/tcc/drakes/dtos/UsuarioListaDTO.java
@@ -0,0 +1,39 @@
+
+package com.tcc.drakes.dtos;
+
+public class UsuarioListaDTO {
+
+    private Long id;
+    private String nome;
+    private int pontuacao;
+
+    public UsuarioListaDTO(Long id, String nome, int pontuacao) {
+        this.id = id;
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+}
